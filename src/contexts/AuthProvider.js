1c38f5ff5c8dfe702347735e700394e3b14d9c20
Waheed-Labib <labibwahid28@gmail.com>
2023-05-28 20:17:15 +0600

    firebase installation, firebase configuration and implementation of env

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,6 +1,9 @@
 import React, { createContext, useState } from 'react';
+import { getAuth } from 'firebase/auth'
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
 
